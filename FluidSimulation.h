7421be3ceb98c16da8aa9ef673b6b6d5d273f262
silver <53583492+SilverArgente@users.noreply.github.com>
2024-11-03 21:57:13 -0500

    Implemented fluid sim algorithms

--- FluidSimulation.h
+++ FluidSimulation.h
@@ -1,30 +1,38 @@
 #ifndef FLUIDSIM_H
 #define FLUIDSIM_H
 #define N 20
-#define IX(i, j, k) ((k + (j * (N+2))) + i * (N+2) * (N+2))
+#define IX(i, j) (i  + (N+2)*j)
 
 class FluidSimulation {
 public:
     int n;
-    int isSimulating = 0;
-    float* vel_x, * vel_y, * vel_z;
-    float* vel_x_prev, * vel_y_prev, * vel_z_prev;
+    float* vel_x, * vel_y;
+    float* vel_x_prev, * vel_y_prev;
     float* dens, * dens_prev;
 
     FluidSimulation()
     {
+        int size = (N + 2) * (N + 2);
+        
         n = N;
+        dens = new float[size];
+        dens_prev = new float[size];
+
+        vel_x = new float[size];
+        vel_y = new float[size];
+        vel_x_prev = new float[size];
+        vel_y_prev = new float[size];    
     }
 
-    void vel_step(int n, float* vel_x, float* vel_y, float* vel_z, float* vel_x_prev, float* vel_y_prev, float* vel_z_prev, float visc, float dt);
-    void dens_step(int n, float *dens, float* dens_prev, float* vel_x, float* vel_y, float* vel_z, float diff, float dt);
-
+    void set_bnd(int n, int b, float* x);
     void add_source(int n, float* x, float* s, float dt);
     void diffuse(int n, int b, float* x, float* x0, float diff, float dt);
-    void advect(int n, int b, float* d, float* d0, float* vel_x, float* vel_y, float* vel_z, float dt);
-    void project(int n, float* vel_x, float* vel_y, float* vel_z, float *p, float *div);
-    void set_bnd(int n, int b, float* x);
+    void advect(int n, int b, float* d, float* d0, float* vel_x, float* vel_y, float dt);
+    void project(int n, float* vel_x, float* vel_y, float* p, float* div);
+    void vel_step(int n, float* vel_x, float* vel_y, float* vel_x_prev, float* vel_y_prev, float visc, float dt);
+    void dens_step(int n, float* dens, float* dens_prev, float* vel_x, float* vel_y, float diff, float dt);
     
+
 };
 
 #endif // FLUIDSIM_H
